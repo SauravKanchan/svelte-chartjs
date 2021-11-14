31a52b3f0995f52515f4936321df223d81f3d8b9
michmich112 <cantacuzene.michel@gmail.com>
2021-11-14 12:30:37 -0800

    final types v1.1.1

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,18 @@
 import { SvelteComponentTyped } from "svelte";
-import { ChartConfiguration } from "chart.js";
+import { ChartConfiguration, ChartData, ChartOptions } from "chart.js";
+
+interface NoTypeChartConfiguration {
+  data: ChartData,
+  options?: ChartOptions,
+}
 
 export declare class Base extends SvelteComponentTyped<ChartConfiguration>{ }
-export declare class Line extends Base { }
-export declare class Radar extends Base { }
-export declare class Doughnut extends Base { }
-export declare class Pie extends Base { }
-export declare class Bar extends Base { }
-export declare class HorizontalBar extends Base { }
-export declare class Scatter extends Base { }
-export declare class Bubble extends Base { }
-export declare class Polar extends Base { }
+export declare class Line extends SvelteComponentTyped<NoTypeChartConfiguration>{ }
+export declare class Radar extends SvelteComponentTyped<NoTypeChartConfiguration>{ }
+export declare class Doughnut extends SvelteComponentTyped<NoTypeChartConfiguration>{ }
+export declare class Pie extends SvelteComponentTyped<NoTypeChartConfiguration>{ }
+export declare class HorizontalBar extends SvelteComponentTyped<NoTypeChartConfiguration>{ }
+export declare class Scatter extends SvelteComponentTyped<NoTypeChartConfiguration>{ }
+export declare class Bubble extends SvelteComponentTyped<NoTypeChartConfiguration>{ }
+export declare class Polar extends SvelteComponentTyped<NoTypeChartConfiguration>{ }
 
