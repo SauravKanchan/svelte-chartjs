465b8e09b604c70b03a8df0a70ee9e0e44f25372
michmich112 <cantacuzene.michel@gmail.com>
2021-11-13 20:21:57 -0800

    initial implementation for types

---
+++ types/index.d.ts
@@ -0,0 +1,14 @@
+import { SvelteComponentTyped } from "svelte";
+import { ChartConfiguration } from "chart.js";
+
+export declare class Base extends SvelteComponentTyped<ChartConfiguration>{ }
+export declare class Line extends Base { }
+export declare class Radar extends Base { }
+export declare class Doughnut extends Base { }
+export declare class Pie extends Base { }
+export declare class Bar extends Base { }
+export declare class HorizontalBar extends Base { }
+export declare class Scatter extends Base { }
+export declare class Bubble extends Base { }
+export declare class Polar extends Base { }
+
