9c35f2700858b9aa4701415d2d0cbd57e67ac1e2
v.terekhov <v.terekhov@iportal.ru>
2022-06-23 14:38:41 +0400

    build: add clean-publish and tune build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,45 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
-
-const production = !process.env.ROLLUP_WATCH;
 
 const name = pkg.name
 	.replace(/^\w/, m => m.toUpperCase())
 	.replace(/-\w/g, m => m[1].toUpperCase()); // > SvelteFrappeCharts
 
+const extensions = ['.js', '.svelte'];
+const external = _ => /node_modules/.test(_);
+const plugins = [ nodeResolve({extensions}), svelte()];
 
-export default {
-	input: 'src/index.js',
-	output: [
-		{
-			file: pkg.module,
-			'format': 'es'
-		},
-		{
-			file: pkg.main,
-			'format': 'umd',
+export default [
+  {
+    input: pkg.main,
+    plugins,
+    external,
+    output: {
+      format: 'cjs',
+      file: pkg.publishConfig.main,
+      exports: 'named',
+      sourcemap: true
+    },
+  },
+  {
+    input: pkg.main,
+    plugins,
+    external,
+    output: {
+      format: 'es',
+      file: pkg.publishConfig.module,
+      sourcemap: true
+    },
+  },
+  {
+    input: pkg.main,
+    plugins: [...plugins, commonjs()],
+    output: {
+			format: 'umd',
+      file: pkg.publishConfig.unpkg,
 			name
 		}
-	],
-	plugins: [
-		svelte(),
-		resolve(),
-		commonjs()
-	]
-};
+  }
+];
