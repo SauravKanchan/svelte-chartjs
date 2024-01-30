c850b88352560376224e086a3bd3b66e0c7e09d5
Kieran Boyle <norcal.kieran@gmail.com>
2024-01-29 21:01:16 -0800

    fix(size): update size test to use the new build

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -9,19 +9,19 @@ function modifyWebpackConfig(config) {
 
 module.exports = [
   {
-    path: 'package/index.js',
+    path: 'dist/index.js',
     limit: '840 B',
     import: '{ Chart }',
     modifyWebpackConfig
   },
   {
-    path: 'package/index.js',
+    path: 'dist/index.js',
     limit: '1.25 KB',
     import: '{ Bar }',
     modifyWebpackConfig
   },
   {
-    path: 'package/index.js',
+    path: 'dist/index.js',
     limit: '1.55 KB',
     import: '{ Bar, Bubble, Chart, Doughnut, Line, Pie, PolarArea, Radar, Scatter }',
     modifyWebpackConfig
